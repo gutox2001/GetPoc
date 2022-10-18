45ef0e25d09d98e56aa33349d663a67c44b1cd9f
Bigode <advmoraisgabriel@gmail.com>
2022-10-18 15:57:41 -0300

    Inclusão de modelos, armazenamentos e visão

---
+++ src/main/java/br/ufv/caf/armazenamento/ListaPocs.java
@@ -0,0 +1,34 @@
+package br.ufv.caf.armazenamento;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import br.ufv.caf.modelo.Poc;
+
+public class ListaPocs {
+    private ArrayList<Poc> listaPocs= new ArrayList<>();
+
+    public ArrayList<Poc> getListaPocs(){
+        return this.listaPocs;
+    }
+    
+    public void adicionar(Poc poc){
+        this.listaPocs.add(poc);
+    }
+
+    public void remover(String tituloPoc){
+        Poc pEliminar = new Poc(tituloPoc, null, tituloPoc, tituloPoc, null, tituloPoc, null);
+        pEliminar = pesquisar(tituloPoc);
+        this.listaPocs.remove(pEliminar);
+    }
+
+    public Poc pesquisar(String tituloPoc){
+        for (Poc poc : listaPocs) {
+            if (Objects.equals(poc.getTitulo(), tituloPoc)){
+                return poc;
+            }
+        }
+        return null;
+    }
+    
+}
