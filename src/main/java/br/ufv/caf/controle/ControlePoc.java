9b09985184cde8744d6e768bbb0f3284d47f9f83
gutox2001 <aroldo.simoes@ufv.br>
2022-11-09 00:30:31 -0300

    Separação dos Controles e Criação dos Testes(NF)

--- src/main/java/br/ufv/caf/controle/ControlePoc.java
+++ src/main/java/br/ufv/caf/controle/ControlePoc.java
@@ -1,105 +1,26 @@
 package br.ufv.caf.controle;
 
-import br.ufv.caf.armazenamento.ArmazenamentoPocs;
-import br.ufv.caf.armazenamento.ArmazenamentoUsuarios;
-import br.ufv.caf.modelo.Poc;
-import br.ufv.caf.modelo.Usuario;
-import br.ufv.caf.visao.MenusControle;
-
 import java.util.ArrayList;
+import br.ufv.caf.armazenamento.ArmazenamentoPocs;
+import br.ufv.caf.modelo.Poc;
 
-//TODO colocar no cabeçalho as descrições das funções
-//TODO fazer modulo de validação
-/*
- *
- * Classe que faz o controle do fluxo de informçãoes do sistema do GetPoc;
- * Métodos:
- *  public ControleGetPoc - construtor da classe;
- *
- *  @Aroldo Augusto Barbosa Simões - 4250
- *  @Gabriel Ryan dos Santos Oliveira - 4688
- *  @Thiago Cândido Rocha - 4225
- *  @João Vitor Chagas Lobo - 4693
- *
- * @since 02/11/2022 - 16:00
- *
- */
-
-public class ControleGetPoc extends MenusControle {
-
-    ArmazenamentoUsuarios armzUsuarios;
-    ArmazenamentoPocs armzPocs;
-
-    public ControleGetPoc()  {
-        super();
-        armzUsuarios = new ArmazenamentoUsuarios();
-    }
-
-    //TODO - ANOTAÇÃO a principio essas funções ficarão aqui, mas talvez seja melhor fazer 2 controles separados
-    /** --- Funções de Usuário ---
-     * Manipulam a classe abstrata Usuario e as suas subclasses;
-     * Todos os usuários são identificados por suas matrículas
-     */
-
-    public void addUsuario(Usuario usuarioAdm, Usuario novoUsuario) {
-
-        if (!armzUsuarios.pesquisaUsuario(novoUsuario)){
-            armzUsuarios.addUsuario(novoUsuario);
-
-        } else {
-            verificaCadastroUsuario(true, novoUsuario);
-
-        }
-    }
-
-    public void removeUsuario(Usuario usuarioAdm, Usuario usuarioARemover) {
-
-        if (armzUsuarios.pesquisaUsuario(usuarioARemover)) {
-                armzUsuarios.removeUsuario(usuarioARemover);
-
-        } else {
-            verificaCadastroUsuario(false, usuarioARemover);
-
-        }
-    }
-
-    public void pesquisaUsuario(Usuario usuarioAPesquisar) {
-        boolean flag = armzUsuarios.pesquisaUsuario(usuarioAPesquisar);
-
-        verificaCadastroUsuario(flag, usuarioAPesquisar);
-    }
-
-    public void exibirTodosUsuarios() {
-
-        if (armzUsuarios.isEmpty()){
-            sistemaSemUsuarios();
-
-
-        } else {
-            ArrayList<Usuario> usuariosCadastrados = armzUsuarios.getListaUsuarios();
-
-            //TODO - TELA verificar se é o ideal passar a lista para a visão
-
-            exibeUsuariosDoSistema(usuariosCadastrados);
-
-            /*for (Usuario usuario : usuariosCadastrados) {
-                System.out.println(usuario);
-
-            }*/
-        }
-    }
-
+public class ControlePoc {
     /** --- Funções de Poc ---
      * Manipulam a classe Poc;
      */
+
+    ArmazenamentoPocs armzPocs;
+
+    public ControlePoc(){
+        armzPocs = new ArmazenamentoPocs();
+    }
 
     public void addPoc(Poc novoPoc) {
 
         if (!armzPocs.pesquisaPoc(novoPoc)){
             armzPocs.addPoc(novoPoc);
-
         } else {
-            verificaCadastroPOC(true, novoPoc);
+            //verificaCadastroPOC(true, novoPoc);
 
         }
     }
@@ -109,28 +30,30 @@ public class ControleGetPoc extends MenusControle {
             armzPocs.removePoc(pocARemover);
 
         } else {
-            verificaCadastroPOC(false, pocARemover);
+            //verificaCadastroPOC(false, pocARemover);
 
         }
     }
 
-    public void pesquisarPoc(Poc pocAPesquisar) {
+    public boolean pesquisarPoc(Poc pocAPesquisar) {
 
-        boolean flag = armzPocs.pesquisaPoc(pocAPesquisar);
-
-        verificaCadastroPOC(flag, pocAPesquisar);
+        //boolean flag =
+        //verificaCadastroPOC(flag, pocAPesquisar);
+        
+        return armzPocs.pesquisaPoc(pocAPesquisar);
     }
 
-    public void exibirPocs() {
+    public void exibirPocs() { //TODO - Melhor retornar POCs n? //Aroldo
 
         if (armzPocs.isEmpty()){
-            sistemaSemPOCs();
+            //sistemaSemPOCs();
+            System.out.println("Sistema não possui POCs cadastradas!");
 
         } else {
             ArrayList<Poc> pocsCadastrados = armzPocs.getPocs();
 
             //TODO - TELA verificar se é o ideal passar a lista de POCs para a visão
-            exibePocsDoSistema(pocsCadastrados);
+            //exibePocsDoSistema(pocsCadastrados);
             /*for (Poc pocs : pocsCadastrados) {
                 System.out.println(pocs);
 
--- src/test/java/br/ufv/caf/PocTest.java
+++ src/test/java/br/ufv/caf/PocTest.java
@@ -1,5 +1,161 @@
 package br.ufv.caf;
 
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.Test;
+//import java.util.ArrayList;
+import br.ufv.caf.controle.ControlePoc;
+import br.ufv.caf.modelo.Poc;
+
+
+/*
+*
+*Classe que faz o teste das classes Poc e ListaPocs;
+*Métodos:
+*	public TestePoc - construtor da classe;
+*	public void iniciaTeste - inicializa uma lista de Poc, uma lista de autores e uma lista de palavras chaves;
+*   public void teste1, public void teste2, public void teste3 - faz o teste das classes Poc e ListaPocs;
+*
+*
+*
+*@Gabriel Ryan dos Santos Oliveira - 4688
+*
+*/
+
 public class PocTest {
-    
+    ControlePoc controlePoc = new ControlePoc();
+
+    public PocTest(){}
+
+
+    @BeforeEach
+    public void iniciaTeste() {
+        /*
+        ArrayList<String> listaAutores = new ArrayList<String>();
+        ArrayList<String> listaPalavrasChave = new ArrayList<String>();
+
+        //PRIMEIRA POC
+        listaAutores.add("Aroldo");
+        listaAutores.add("Gabriel");
+
+        listaPalavrasChave.add("ESOF");
+        listaPalavrasChave.add("Engenharia");
+        listaPalavrasChave.add("SoftWare");
+        */
+        controlePoc.addPoc(new Poc("ESOF", null, "Aroldo",
+        "Gabriel", null, "ENGENHARIA_DE_SOFTWARE", 
+        Poc.Area.ENGENHARIA_DE_SOFTWARE));
+        /*listaAutores.clear();
+        listaPalavrasChave.clear();    */
+
+        //SEGUNDA POC
+        /* 
+        listaAutores.add("Joao");
+        listaAutores.add("Thiago");
+
+        listaPalavrasChave.add("CD");
+        listaPalavrasChave.add("Ciencia");
+        listaPalavrasChave.add("Dados");
+        */
+        controlePoc.addPoc(new Poc("CD", null, "Joao",
+        "Thiago", null, "CIENCIA_DE_DADOS", 
+        Poc.Area.CIENCIA_DE_DADOS));
+        /*/
+        listaAutores.clear();
+        listaPalavrasChave.clear();*/
+
+        //TERCEIRA POC
+        /*
+        listaAutores.add("Gabriel");
+        listaAutores.add("Joao");
+
+        listaPalavrasChave.add("IC");
+        listaPalavrasChave.add("Internet");
+        listaPalavrasChave.add("Coisas");*/
+
+        controlePoc.addPoc(new Poc("IC", null, "Gabriel",
+        "Joao", null, "INTERNET_DAS_COISAS", 
+        Poc.Area.INTERNET_DAS_COISAS));
+        /*
+        listaAutores.clear();
+        listaPalavrasChave.clear();*/
+    }
+
+    @Test
+    public void teste01(){
+        iniciaTeste();
+        //controlePoc.editar();
+        /*
+        ArrayList<String> listaAutoresTeste1 = new ArrayList<String>();
+        ArrayList<String> listaPalavrasChaveTeste1 = new ArrayList<String>();
+        
+        listaAutoresTeste1.add("Aroldo");
+        listaAutoresTeste1.add("Gabriel");
+
+        listaPalavrasChaveTeste1.add("ESOF");
+        listaPalavrasChaveTeste1.add("Engenharia");
+        listaPalavrasChaveTeste1.add("SoftWare");*/
+
+        Poc poc1 = new Poc("ESOF", null, "Aroldo",
+        "Gabriel", null, "ENGENHARIA_DE_SOFTWARE", 
+        Poc.Area.ENGENHARIA_DE_SOFTWARE);
+
+        assertEquals(true, controlePoc.pesquisarPoc(poc1));
+
+        controlePoc.removePoc(poc1);
+        assertEquals(controlePoc.pesquisarPoc(poc1), null );
+
+        //controlePoc.editar();
+    }
+
+    @Test
+    public void teste2() {
+        iniciaTeste();
+        /*
+        ArrayList<String> listaAutoresTeste2 = new ArrayList<String>();
+        ArrayList<String> listaPalavrasChaveTeste2 = new ArrayList<String>();
+        listaAutoresTeste2.add("Joao");
+        listaAutoresTeste2.add("Thiago");
+
+        listaPalavrasChaveTeste2.add("CD");
+        listaPalavrasChaveTeste2.add("Ciencia");
+        listaPalavrasChaveTeste2.add("Dados");*/
+
+        Poc poc2 = new Poc("CD", null, "Joao",
+        "Thiago", null, "CIENCIA_DE_DADOS", 
+        Poc.Area.CIENCIA_DE_DADOS);
+
+        assertEquals(true, controlePoc.pesquisarPoc(poc2));
+
+        controlePoc.removePoc(poc2);
+        assertEquals(controlePoc.pesquisarPoc(poc2), null );
+
+        //controlePoc.editar();
+    }
+
+    @Test
+    public void teste3() {
+        iniciaTeste();
+        /*
+        ArrayList<String> listaAutoresTeste3 = new ArrayList<String>();
+        ArrayList<String> listaPalavrasChaveTeste3 = new ArrayList<String>();
+        
+        listaAutoresTeste3.add("Gabriel");
+        listaAutoresTeste3.add("Joao");
+
+        listaPalavrasChaveTeste3.add("IC");
+        listaPalavrasChaveTeste3.add("Internet");
+        listaPalavrasChaveTeste3.add("Coisas");*/
+
+        Poc poc3 = new Poc("IC", null, "Gabriel",
+        "Joao", null, "INTERNET_DAS_COISAS", 
+        Poc.Area.INTERNET_DAS_COISAS);
+
+        assertEquals(true, controlePoc.pesquisarPoc(poc3));
+
+        controlePoc.removePoc(poc3);
+        assertEquals(controlePoc.pesquisarPoc(poc3), null );
+
+        //controlePoc.editar();
+    }
 }
