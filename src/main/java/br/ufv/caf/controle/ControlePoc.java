546f102bc558e83c5be3bf89c1c43bdb24b49078
joaovclobo <joao.lobo@ufv.br>
2022-12-01 17:21:24 -0300

    Criação toString

--- src/main/java/br/ufv/caf/controle/ControlePoc.java
+++ src/main/java/br/ufv/caf/controle/ControlePoc.java
@@ -141,69 +141,6 @@ public class ControlePoc {
         else {
             return false; //Poc desatualizada não foi encontrada;
         }
-
-        //TODO: ANALISAR MÉTODO ANTIGO
-        /*int resposta=1, i=1;
-        String tituloEditado, nomeOrientadorEditado, nomeCo_OrientadorEditado, resumoEditado;
-        String areaPocEditado;
-        ArrayList<String> listaAutoresEditado = new ArrayList<>();
-        ArrayList<String> palavras_chaveEditado = new ArrayList<>();
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Entre com o novo titulo: ");
-        tituloEditado = scan.nextLine();
-        setTitulo(tituloEditado);
-
-        System.out.println("Entre com o novo nome do orientador: ");
-        nomeOrientadorEditado = scan.nextLine();
-        setNomeOrientador(nomeOrientadorEditado);
-
-        System.out.println("Entre com o novo nome do co-orientador: ");
-        nomeCo_OrientadorEditado = scan.nextLine();
-        setNomeCo_Orientador(nomeCo_OrientadorEditado);
-
-        System.out.println("Entre com a nova area de atuacao: \nAREAS DISPONIVEIS:");
-        for (Area a : Area.values()) {
-            System.out.println(a.name());
-        }
-        areaPocEditado = scan.nextLine();
-        for (Area a : Area.values()) {
-            if (Objects.equals(a.name(), areaPocEditado)) {
-                setAreaDaPoc(a);
-                break;
-            }
-        }
-
-        do {
-            System.out.println("Entre com o " + i + "º nome do(a) autor(a): ");
-            String nome = scan.nextLine();
-            listaAutoresEditado.add(nome);
-            System.out.println("Deseja inserir outro autor? Digite: 0 para NAO / 1 para SIM");
-            resposta = scan.nextInt();
-            i++;
-            scan.nextLine();
-        } while (resposta==1);
-        setListaAutores(listaAutoresEditado);
-
-        i=1;
-        resposta=1;
-        do {
-            System.out.println("Entre com a "+i+"ª palavra-chave: ");
-            String nome = scan.nextLine();
-            palavras_chaveEditado.add(nome);
-            System.out.println("Deseja inserir outra palavra-chave? Digite: 0 para NAO / 1 para SIM");
-            resposta = scan.nextInt();
-            i++;
-            scan.nextLine();
-        } while (resposta==1);
-        setPalavras_chave(palavras_chaveEditado);
-
-        System.out.println("Entre com o novo resumo: ");
-        resumoEditado = scan.nextLine();
-        setResumo(resumoEditado);
-
-        scan.close();
-    }*/
     }
 
 }
