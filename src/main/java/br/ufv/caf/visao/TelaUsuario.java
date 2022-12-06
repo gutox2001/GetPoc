e1987df6b409512945db34e8c6a47687fed8a69a
gutox2001 <aroldo.simoes@ufv.br>
2022-12-06 17:55:39 -0300

    Pequenas padronizações

--- src/main/java/br/ufv/caf/visao/TelaUsuario.java
+++ src/main/java/br/ufv/caf/visao/TelaUsuario.java
@@ -10,34 +10,38 @@ import br.ufv.caf.modelo.excecoes.ExcecaoUsuarioNaoEncontrado;
 
 import java.util.Scanner;
 
-/** Classe MenusUsuario representa um menu para que os usuários possam ser cadastrados 
+/** Classe TelaUsuario, tela interativa do controlador dos usuários do sistema GetPoc 
  * @author Thiago Cândido Rocha - 4225
  * @since 09/11/2022 - 17:00
  * @version 1.0
  */
 
-public class TelaUsuario{
+public class TelaUsuario {
+
     protected ControleUsuario controle;
     protected Scanner inputUser;
 
-    /** Método MenusUsuario, construtor da classe MenusUsuario
+    /** Método TelaUsuario, construtor da classe TelaUsuario
      * @author Thiago Cândido Rocha - 4225
      * @param controle ControleUsuario - Controle do Usuário
      * @since 09/11/2022 - 17:30
      */
 
-    public TelaUsuario(ControleUsuario controle){
+    public TelaUsuario(ControleUsuario controle) {
+
         this.inputUser = new Scanner(System.in);
         this.controle = controle;
+
     }
     
     /** Método preenchimentoDados, tem a finalidade de preencher os dados do usuário que está sendo cadastrado
      * @author Thiago Cândido Rocha - 4225
-     * @return Usuario - Dados do Usuário
+     * @return Usuario - Usuário a ser cadastrado
      * @since 09/11/2022 - 17:30
      */
 
-    private Usuario preenchimentoDados(){
+    private Usuario preenchimentoDados() {
+
         String nome;
         String matricula;
         String senha;
@@ -57,30 +61,40 @@ public class TelaUsuario{
 
         System.out.println("O usuário é um professor ou um aluno?  \n0 - PROFESSOR   1 - ALUNO");
         opcao = Short.parseShort(this.inputUser.nextLine());
-        if (opcao == 0) {
-            userTemp = new Professor(nome, matricula, senha);
-        } else {
-            userTemp = new Aluno(nome, matricula, senha);
-        }
-        System.out.println("=============================================");
 
+        if (opcao == 0) {
+
+            userTemp = new Professor(nome, matricula, senha);
+
+        } else {
+
+            userTemp = new Aluno(nome, matricula, senha);
+
+        }
+
+        System.out.println("=============================================");
         return userTemp;
 
     }
     
     /** Método validacaoDados, tem a finalidade de validar os dados que serão preenchidos
      * @author Thiago Cândido Rocha - 4225
-     * @return Usuario - Dados do Usuário
+     * @param userTemp Usuario - usuário a ser validade
+     * @return Usuario
      * @since 09/11/2022 - 17:30
      */
     
-    public Usuario validacaoDados(Usuario userTemp){
-        while(!this.controle.validaUsuario(userTemp)){
+    public Usuario validacaoDados(Usuario userTemp) {
+
+        while(!this.controle.validaUsuario(userTemp)) {
+
             System.err.println("Preenchimento correto de dados necessário!");
             preenchimentoDados();
+
         }
 
         return userTemp;
+
     }
 
     /** Método efetuarCadastro, tem a finalidade de efetuar o cadastro do usuário se possível
@@ -91,13 +105,15 @@ public class TelaUsuario{
     private void efetuarCadastro() {
 
         int option = 0;
-
         System.out.println("=============================================");
 
         do {
+
             try {
+
                 this.controle.cadastraUsuario(preenchimentoDados());
                 option = 0;
+
             } catch (ExcecaoDadosInvalidos invalidData) {
 
                 System.out.println("=============================================" +
@@ -108,45 +124,53 @@ public class TelaUsuario{
 
                 option = Integer.parseInt(this.inputUser.nextLine());
 
-
-
-
             } catch (ExcecaoUsuarioJaCadastrado alreadyOn) {
+
                 System.out.println("=============================================" +
                         "=============================================");
                 System.err.println("O usuario ja esta cadastrado no nosso sistema!");
                 System.out.println("=============================================" +
                         "=============================================");
+
             }
 
-        }while(option != 0);
+        } while(option != 0);
+
         System.out.println("=============================================");
 
     }
 
     /** Método efetuarRemocao, tem a finalidade de remover o cadastro do usuário se possível
      * @author Thiago Cândido Rocha - 4225
+     * @param matriculaLogada String - matrícula do usuário logado no sistema
      * @since 09/11/2022 - 18:00
      */
 
     private void efetuarRemocao(String matriculaLogada) {
+
         boolean flag = false;
         String matriculaRemocao;
+
         do {
+
             try {
+
                 System.out.println("Entre com a matrícula do usuário que deseja remover:");
                 matriculaRemocao = this.inputUser.nextLine();
+
                 if(!matriculaRemocao.equals(matriculaLogada)) {
+
                     this.controle.removeUsuario(matriculaRemocao);
                     System.out.println("=============================================" +
                             "=============================================");
                     System.out.println("A remoção foi realizada com sucesso!");
                     System.out.println("=============================================" +
                             "=============================================");
-                }
 
-                else{
+                } else {
+
                     System.err.println("ERRO: Você não pode se remover do sistema!!");
+
                 }
 
                 flag = true;
@@ -158,42 +182,61 @@ public class TelaUsuario{
                 System.err.println("Nao foi possivel encontrar nenhum usuario com estes dados no nosso sistema!");
                 System.out.println("=============================================" +
                         "=============================================");
+
             }
-        }while(!flag);
+        } while(!flag);
 
     }
 
-    private void menuEdicaoAdmin(){
+    /** Método menuEdicaoAdmin, menu para edição de usuários pelos administradores
+     * @author Thiago Cândido Rocha - 4225
+     * @since 09/11/2022 - 17:30
+     */
+
+    private void menuEdicaoAdmin() {
+
         String matricula;
         int option;
         Usuario usuarioPesquisado = null;
 
         do {
+
             try {
+
                 System.out.println("Entre com a matrícula do usuário que deseja editar:");
                 matricula = this.inputUser.nextLine();
                 usuarioPesquisado = this.controle.pesquisaUsuario(matricula);
                 option = 0;
+
             } catch (ExcecaoUsuarioNaoEncontrado notFound) {
+
                 System.err.println("O usuário não está cadastrado no sistema ou a " +
                         "matrícula é inválida, deseja tentar novamente?\n" +
                         "1 - SIM, 0 - NÃO");
                 option = Integer.parseInt(this.inputUser.nextLine());
+
             }
-        }while(option != 0);
+        } while(option != 0);
 
-        if(usuarioPesquisado!=null && usuarioPesquisado.getTipoUsuario().equals(Usuario.TipoUsuario.ADMINISTRADOR)){
+        if(usuarioPesquisado!=null && usuarioPesquisado.getTipoUsuario().equals(Usuario.TipoUsuario.ADMINISTRADOR)) {
+
             System.err.println("Por medidas de segurança, um admin não pode alterar dados de outro admin.");
-        }
 
-        else{
+        } else {
+
             menuEdicaoComum(usuarioPesquisado);
+
         }
-
-
     }
 
-    private void menuEdicaoComum(Usuario usuarioAtual){
+    /** Método menuEdicaoComum, menu para edição do usuário logado
+     * @author Thiago Cândido Rocha - 4225
+     * @param usuarioAtual Usuario - usuário logado no sistema
+     * @since 09/11/2022 - 17:30
+     */
+
+    private void menuEdicaoComum(Usuario usuarioAtual) {
+
         int option = 2;
 
         do {
@@ -207,7 +250,9 @@ public class TelaUsuario{
 
             option = Integer.parseInt(this.inputUser.nextLine());
             switch (option) {
+
                 case 0:
+
                     String nome;
                     System.out.println("Entre com o novo nome de usuário:");
                     nome = this.inputUser.nextLine();
@@ -215,10 +260,14 @@ public class TelaUsuario{
                             "1 - SIM, 0  - NÃO");
 
                     if (Integer.parseInt(this.inputUser.nextLine()) == 1) {
+
                         this.controle.alteraNome(usuarioAtual, nome);
                     }
+
                     break;
+
                 case 1:
+                
                     String senha;
                     System.out.println("Entre com a nova senha:");
                     senha = this.inputUser.nextLine();
@@ -227,39 +276,52 @@ public class TelaUsuario{
                             "1 - SIM, 0  - NÃO");
 
                     if (Integer.parseInt(this.inputUser.nextLine()) == 1) {
+
                         this.controle.alteraSenha(usuarioAtual, senha);
+
                     }
+
                     break;
 
                 default:
+
                     System.out.println("Saindo . . .");
                     break;
+
             }
-        }while(option == 0 || option == 1);
+        } while(option == 0 || option == 1);
     }
 
-    /** Método menuPesquisa, tem a finalidade de pesquisar um Usuário (ADMIN ONLY)
+    /** Método menuExibeBuscaUsuario, tem a finalidade de exibir um usuário pesquisado(ADMINISTRADORES SOMENTE)
      * @author Thiago Cândido Rocha - 4225
      * @since 09/11/2022 - 18:00
      */
     
-    private void menuExibeBuscaUsuario(){
+    private void menuExibeBuscaUsuario() {
+
         System.out.println("Entre com a matrícula do usuário que deseja pesquisar");
-        try{
+
+        try {
+
             Usuario usuarioPesquisado = this.controle.pesquisaUsuario(this.inputUser.nextLine());
             System.out.println(usuarioPesquisado.toString());
-        }catch(ExcecaoUsuarioNaoEncontrado notFound){
-            System.err.println("O usuário procurado não existe!");
-        }
 
+        } catch(ExcecaoUsuarioNaoEncontrado notFound) {
+
+            System.err.println("O usuário procurado não existe!");
+
+        }
     }
     
     /** Método menuFuncionalidadesAluno, tem a finalidade de mostrar as funcionalidades de um aluno
      * @author Thiago Cândido Rocha - 4225
+     * @param telaPoc TelaPoc - menus de Pocs
+     * @param usuarioLogado Usuario - usuário logado no sistema
      * @since 09/11/2022 - 18:00
      */
 
     protected void menuFuncionalidadesAluno(TelaPoc telaPoc, Usuario usuarioLogado) {
+
         int opcao;
         String nomePoc;
 
@@ -267,6 +329,7 @@ public class TelaUsuario{
         telaPoc.exibeTodosOsTitulosPocs();
 
         do {
+
             System.out.println("=============================================");
             System.out.println("| Funcionalidades disponiveis:               |");
             System.out.println("| o 0 -> Sair                                |");
@@ -276,28 +339,38 @@ public class TelaUsuario{
 
             System.out.print("-> ");
             opcao = Integer.parseInt(inputUser.nextLine());
+
             switch (opcao) {
+
                 case 0:
+
                     System.out.println("OBRIGADO!");
                     break;
+
                 case 2:
+
                     this.menuEdicaoComum(usuarioLogado);
                     break;
 
                 default: //Assume que valores incorretos sempre irão pesquisar
+
                     telaPoc.menuPesquisa();
                     break;
+
             }
         } while (opcao != 0);
+
     }
 
     /** Método menuFuncionalidadesProfessor, tem a finalidade de mostrar as opções de um professor
      * @author Thiago Cândido Rocha - 4225
-     * @param telaPoc TelaPoc - visão do POC
+     * @param telaPoc TelaPoc - menus de Pocs
+     * @param usuarioLogado Usuario - usuário logado no sistema
      * @since 09/11/2022 - 18:00
      */
 
     protected void menuFuncionalidadesProfessor(TelaPoc telaPoc, Usuario usuarioLogado) {
+
         int opcao;
         String nomePoc;
 
@@ -305,6 +378,7 @@ public class TelaUsuario{
         telaPoc.exibeTodosOsTitulosPocs();
 
         do {
+
             System.out.println("=============================================");
             System.out.println("| Funcionalidades disponiveis:               |");
             System.out.println("| o 0 -> Sair                                |");
@@ -319,42 +393,58 @@ public class TelaUsuario{
             opcao = Integer.parseInt(inputUser.nextLine());
 
             switch (opcao) {
+
                 case 1:
+
                     telaPoc.menuPesquisa();
                     break;
+
                 case 2:
+
                     telaPoc.menuCadastro(usuarioLogado.getMatricula());
                     break;
+
                 case 3:
+
                     telaPoc.menuEdicao(usuarioLogado.getMatricula(), false);
                     break;
+
                 case 4:
+
                     telaPoc.menuRemocao();
                     break;
+
                 case 5:
+
                     this.menuEdicaoComum(usuarioLogado);
+
                 default:
+
                     System.out.println("OBRIGADO!");
                     break;
+
             }
         } while (opcao != 0);
+
     }
 
     /** Método menuFuncionalidadesAdministrador, tem a finalidade de mostrar as opções de um administrador
      * @author Thiago Cândido Rocha - 4225
      * @param telaPoc TelaPoc - visão do POC
+     * @param matriculaLogada String - matrícula do usuário logado no sistema
      * @since 09/11/2022 - 18:00
      */
 
     protected void menuFuncionalidadesAdministrador(TelaPoc telaPoc,
                                                     String matriculaLogada) {
+
         int opcao;
         String nomePoc;
 
         System.out.print("\n\n");
         System.out.println("=============================================" +
-                "============================================" +
-                "==============================================");
+                    "============================================" +
+                    "==============================================");
         System.out.println("Lista dos Pocs cadastrados no sistema:");
         telaPoc.exibeTodosOsTitulosPocs();
         System.out.print("\n\n");
@@ -362,8 +452,8 @@ public class TelaUsuario{
                 "============================================" +
                 "==============================================");
 
-
         do {
+
             System.out.println("=============================================");
             System.out.println("| Funcionalidades disponiveis:               |");
             System.out.println("| o 0 -> Sair                                |");
@@ -379,38 +469,56 @@ public class TelaUsuario{
 
             System.out.print("-> ");
             opcao = Integer.parseInt(inputUser.nextLine());
+
             switch (opcao) {
+
                 case 1:
+
                     telaPoc.menuPesquisa();
                     break;
+
                 case 2:
+
                     telaPoc.menuCadastro(matriculaLogada);
                     break;
+
                 case 3:
+
                     telaPoc.menuEdicao(matriculaLogada, true);
                     break;
+
                 case 4:
+
                     telaPoc.menuRemocao();
                     break;
+
                 case 5: 
+
                     this.menuExibeBuscaUsuario();
                     break;
+
                 case 6:
+
                     this.efetuarCadastro();
                     break;
+
                 case 7:
+
                     this.menuEdicaoAdmin();
                     break;
+
                 case 8:
+
                     this.efetuarRemocao(matriculaLogada);
                     break;
 
-
                 default:
+
                     System.out.println("OBRIGADO!");
                     break;
+
             }
         } while (opcao != 0);
-    }
-    
+
+    }   
 }
